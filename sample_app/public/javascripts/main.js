3966a078debe5048373dd1a349386937f0d56b97
jandritsch <jandritsch@gettyimages.com>
2014-04-05 11:33:08 -0500

    Comment out sample code in main.js so the sample_app works out of the box

--- sample_app/public/javascripts/main.js
+++ sample_app/public/javascripts/main.js
@@ -59,13 +59,16 @@ $(document).ready(function() {
     // Here is an opportunity to make any changes about the uploader's configuration.
     // For instance, if you wanted to dynamically supply an access key or bucket
     // from the back end, you could do that here.
-    $.get('/someplace', function(data) {
-      uploader.settings.bucket       = data.bucket;
-      uploader.settings.awsAccessKey = data.awsAccessKey;
-      uploader.startUpload();
-    });
+    /*
+        $.get('/someplace', function(data) {
+          uploader.settings.bucket       = data.bucket;
+          uploader.settings.awsAccessKey = data.awsAccessKey;
+          uploader.startUpload();
+        });
+    */
     // It should also be noted that if you dynamically modify the 'bucket', you will also need to
     // modify the 'host', as it is derived from the bucket at the time an uploader is created.
+    uploader.startUpload();
   });
 
   $("#cancelUpload").on("click", function() {
