bf5fb9fa0620664e70481a43b9eefa2237de6a71
jandritsch <jandritsch@gettyimages.com>
2014-12-04 14:32:55 -0600

    Ensure the proper context is available within the worker

--- dist/basic_s3_worker.js
+++ dist/basic_s3_worker.js
@@ -4,20 +4,22 @@
 *
 * The data coming in should be in the following format:
 *
-*   { 
+*   {
 *     text: "the text to encrypt",
 *     uploaderFilePath: "/path/to/basic_s3_uploader.js"
 *   }
 */
 self.addEventListener('message', function(e) {
-  // Import the uploader, which contains the 
-  // asmCrypto library.
-  self.importScripts(e.data.uploaderFilePath);
+  // Make sure we have the proper scope available
+  if (typeof self.importScripts === 'function') {
+    // Import the uploader, which contains the
+    // asmCrypto library.
+    self.importScripts(e.data.uploaderFilePath);
 
-  // Encrypt the value
-  var encrypted = asmCrypto.SHA256.hex(e.data.text);
+    // Encrypt the value
+    var encrypted = asmCrypto.SHA256.hex(e.data.text);
 
-  // Send the encrypted data back
-  self.postMessage(encrypted);
-
+    // Send the encrypted data back
+    self.postMessage(encrypted);
+  }
 }, false);
